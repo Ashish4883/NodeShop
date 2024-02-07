663b7265d62b06a4bc94aa498f0d184c50475057
Ashish4883 <aashishmishra0016@gmail.com>
2024-02-07 22:40:48 +0530

    Added FrontEnd Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1>Welcome to NodeShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet></Outlet>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default App;
