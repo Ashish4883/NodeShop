20aa2779de6bfc58e3917db87dd951247a31538b
Ashish4883 <aashishmishra0016@gmail.com>
2024-02-22 21:38:28 +0530

    Added Backend Authentication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(protect, admin, getUsers).post(registerUser);
+router.post("/logout", logoutUser);
+router.post("/auth", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,27 @@ dotenv.config();
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 connectDB(); //Connect to MongoDB
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookkie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
